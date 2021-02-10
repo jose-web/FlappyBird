f813c3f0f1a7286abce0122fe19a65c7003eea16
jose <delossantosramosjose@gmail.com>
2021-02-10 22:01:16 +0100

    Se ha creado el script para controlar al jugador

---
+++ Jugador.gd
@@ -0,0 +1,31 @@
+extends RigidBody2D
+
+export var FLAP_FORCE = -200
+
+const MAX_ROTATION_DEGREES = -30
+
+onready var animator = $AnimationPlayer
+
+var started = false
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_up"):
+		if !started:
+			start()
+		flap()
+	if rotation_degrees <= MAX_ROTATION_DEGREES:
+		angular_velocity = 0
+		rotation_degrees = MAX_ROTATION_DEGREES
+	
+	if linear_velocity.y > 0:
+		angular_velocity = 3 if rotation_degrees <= 90 else 0
+
+func start():
+	if started: return
+	started = true
+	gravity_scale = 5
+	animator.play("Alas")
+
+func flap():
+	linear_velocity.y = FLAP_FORCE
+	angular_velocity = -8
