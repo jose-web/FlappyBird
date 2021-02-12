d5cda129ef425ceecd8aaecc8ee233ef9629af3a
jose <delossantosramosjose@gmail.com>
2021-02-13 00:54:38 +0100

    Se ha añadido la función para detectar si el jugador se choca con el tubo o suma punto

--- Jugador.gd
+++ Jugador.gd
@@ -1,4 +1,5 @@
 extends RigidBody2D
+class_name Jugador
 
 export var FLAP_FORCE = -200
 
--- Obstaculo.gd
+++ Obstaculo.gd
@@ -5,3 +5,14 @@ func _physics_process(delta):
 	position.x += -VELOCIDAD * delta
 	if global_position.x <= -200:
 		global_position.x = 500
+
+
+
+func _on_Tubo_body_entered(body):
+	if body is Jugador:
+		print("choca con el tubo")
+
+
+func _on_AreaSumaPunto_body_entered(body):
+	if body is Jugador:
+		print("Suma puntos")
